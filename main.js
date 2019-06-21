e721a9621f9b1eb5329773d41c2f10ac4f396283
Chris Dudziak <chris.dudziak@gmail.com>
2019-06-21 18:40:56 -0400

    Bowl Rollover

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ var gameData = {
   bowlPerClick: 1
 }
 
-function throwBall() {
-  gameData.bowl += gameData.bowlPerClick
-document.getElementById("overCount").innerHTML = gameData.over + "." + gameData.bowl
+function throwBall() {  
+  gameData.bowl += gameData.bowlPerClick;
+
+  if (gameData.bowl > 5) { 
+    gameData.over++;
+    gameData.bowl = 0; 
+  } 
+  
+  document.getElementById("overCount").innerHTML = gameData.over + "." + gameData.bowl;
 }
 
 //if bowl=6, gameData.over += 1, gameData.bowl -= 6
