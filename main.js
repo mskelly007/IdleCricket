ce457dc6275c56fe38ec45137f8001b80ea609b6
mskelly007 <mskelly.007@gmail.com>
2019-06-24 07:41:37 +0000

    added random bowl results; needs fixing

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ var gameData = {
   overB: 0,
   bowlA: 0,
   bowlB: 0,
-  runsA: 200,
+  runsA: 0,
   runsB: 0,
+  outsA: 0,
+  outsB: 0,
   bowlPerClick: 1
 }
 
 function generatePlayers() {
     var players = [{}];
 
-    for (i = 0; i < 1000; i++) {        
+    for (i = 0; i < 1000; i++) {
         var player = {
             pitchModifier: Math.random(),
             runModifier: Math.random(),
@@ -22,11 +24,10 @@ function generatePlayers() {
             player
         );
     }
-    
+
     console.log(players);
 }
 
-//I broke the click aspect out into it's own function so you could reset the timer of the main function.
 function clickThrowBall() {
     if (gameData.overB == 50){
         return;
@@ -38,17 +39,24 @@ function clickThrowBall() {
 }
 
 function throwBall() {
-    if (gameData.overA>=50) {
-        gameData.bowlB += gameData.bowlPerClick;
-
-        if (gameData.bowlB > 5) {
-            gameData.overB++;
-            gameData.bowlB = 0;
+//defining the result of a bowl
+/*
+    var bowlResult: Math.random(),
+    if (bowlResult <= .25){
+        gameData.runsA ++
+    }
+    else {
+        if (bowlresult >= .75) {
+            gameData.outsA ++
         }
+        else {
+            return;
+        }
+    }
+*/
 
-        document.getElementById("overCountB").innerHTML = gameData.overB + "." + gameData.bowlB;
-        document.getElementById("crrB").innerHTML = gameData.runsB / gameData.overB;
-    } else {
+//updating the current over count
+    if (gameData.overA<50) {
         gameData.bowlA += gameData.bowlPerClick;
 
         if (gameData.bowlA > 5) {
@@ -59,28 +67,32 @@ function throwBall() {
         var overCountA = gameData.overA + "." + gameData.bowlA;
         document.getElementById("overCountA").innerHTML = overCountA;
 
+        document.getElementById("runsA").innerHTML = runsA;
+
         var crrA = gameData.runsA / (gameData.overA + (gameData.bowlA/6));
         document.getElementById("crrA").innerHTML = crrA;
     }
-    
+    else {
+        gameData.bowlB += gameData.bowlPerClick;
+
+        if (gameData.bowlB > 5) {
+            gameData.overB++;
+            gameData.bowlB = 0;
+        }
+        var overCountA = gameData.overB + "." + gameData.bowlB;
+        document.getElementById("overCountB").innerHTML = overCountB;
+
+        var crrA = gameData.runsB / (gameData.overB + (gameData.bowlB/6));
+        document.getElementById("crrB").innerHTML = crrB;
+    }
+
+    if(gameData.overB == 50) {
+        return;
+    }
 }
 
-//var mainGameLoop = window.setInterval(function() {
-//  throwBall()
-//}, 5000)
-
-//I also simplified the above to
-//Instead of creating a new function that calls throwball() every 5 seconds, just run throwBall every 5 seconds.
+//Idle Functionality: throws one pitch every 5 seconds
 var mainGameLoop = setInterval(throwBall,5000);
-
-
-
-//I need to make the timer reset on buttonclick
-//I found a thing called clearInterval which I think I just need to add into the throwBall function
-//But everytime I add it into the function, throwBall stops working (i.e the button doesn't click anymore)
-
-
-
 
 /*
 I would think about this:
